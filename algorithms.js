2888e699523884c08a48fea349ea765b62aee9e6
grantge <grantfromgeorgia@gmail.com>
2021-06-26 10:04:54 +0300

    initialize project

---
+++ algorithms.js
@@ -0,0 +1,17 @@
+// Basic math operations
+
+function addition(a, b) {
+  return a + b
+}
+
+function substraction(a, b) {
+  return a - b
+}
+
+function multiplication(a, b) {
+  return a * b
+}
+
+function division(a, b) {
+  return a / b
+}
